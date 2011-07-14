b3cca12d29638da9d63bf3f1d5c895cca27a8099
nubi_matthischke <nubi_matthischke@da8baf20-f473-4a87-bbd7-1904b4eb812b>
2011-07-14 13:26:51 +0000

    removed QoS placeholder
    git-svn-id: file:///home/josef/Work/repos/DBBM+Hiwis/nubi/_NUBICONV/foo@125 da8baf20-f473-4a87-bbd7-1904b4eb812b

--- trunk/gui/Nubisave/src/nubisave/CoreWriter.java
+++ trunk/gui/Nubisave/src/nubisave/CoreWriter.java
@@ -4,14 +4,10 @@
  */
 package nubisave;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
--- trunk/gui/Nubisave/src/nubisave/ui/ServiceEditDialog.java
+++ trunk/gui/Nubisave/src/nubisave/ui/ServiceEditDialog.java
@@ -49,7 +49,6 @@ public class ServiceEditDialog extends javax.swing.JDialog {
         passwdField = new javax.swing.JPasswordField();
         applyBtn = new javax.swing.JButton();
         cancelBtn = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -77,11 +76,6 @@ public class ServiceEditDialog extends javax.swing.JDialog {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("Ubuntu", 0, 24)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("<QoS Paramter>");
-        jLabel3.setPreferredSize(new java.awt.Dimension(97, 30));
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -89,7 +83,6 @@ public class ServiceEditDialog extends javax.swing.JDialog {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 310, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
@@ -119,9 +112,7 @@ public class ServiceEditDialog extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(applyBtn)
                     .addComponent(cancelBtn))
-                .addGap(85, 85, 85)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(75, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -147,7 +138,6 @@ public class ServiceEditDialog extends javax.swing.JDialog {
     private javax.swing.JButton cancelBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPasswordField passwdField;
     private javax.swing.JTextField usernameField;
     // End of variables declaration//GEN-END:variables
