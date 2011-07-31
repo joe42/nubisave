52d69f2c1c081c06752065afaebc09a50c29f00e
nubi_mueller <nubi_mueller@da8baf20-f473-4a87-bbd7-1904b4eb812b>
2011-07-31 12:28:58 +0000

    Added general fuse test shell script: fuse_tests.sh
    git-svn-id: file:///home/josef/Work/repos/DBBM+Hiwis/nubi/_NUBICONV/foo@199 da8baf20-f473-4a87-bbd7-1904b4eb812b

---
+++ trunk/testsuite/fuse_tests.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+short="short"
+long=longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong
+path="$1/$2"
+
+
+##########################################################
+echo "Creating new empty file"
+##########################################################
+touch "$path"
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+
+
+##########################################################
+echo "Removing empty file"
+##########################################################
+rm "$path"
+if [ -f "$path" ]
+then
+    echo "Error: File does exists."
+fi
+
+##########################################################
+echo "Reading empty file"
+##########################################################
+touch "$path"
+if [ "`cat "$path"`" != '' ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+
+##########################################################
+echo "Appending to empty file and reading"
+##########################################################
+echo $short >> "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $short\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+echo $long >> "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+
+##########################################################
+echo "Truncating empty file and reading"
+##########################################################
+echo $short > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $short\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+echo $long > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+
+##########################################################
+echo "Appending to non empty file and reading"
+##########################################################
+echo $short >> "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $short\\n)" ] 
+then
+	echo "Error: Files are not equal 1."
+fi
+echo $long >> "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $short\\n$long\\n)" ] 
+then
+	echo "Error: Files are not equal 2."
+fi
+rm "$path"
+
+
+##########################################################
+echo "Truncating non empty file and reading"
+##########################################################
+echo $short >> "$path" 
+echo $short > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $short\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+echo $long >> "$path" 
+echo $long > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+
+##########################################################
+echo "Moving empty file"
+##########################################################
+touch "$path"
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+mv "$path" "$path renamed"
+if [ ! -f "$path renamed" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ -f "$path" ]
+then
+    echo "Error: File does exists."
+fi
+if [ "`cat "$path renamed"`" != "" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path renamed"
+
+
+##########################################################
+echo "Moving non empty file"
+##########################################################
+echo $long > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+mv "$path" "$path renamed"
+if [ ! -f "$path renamed" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ -f "$path" ]
+then
+    echo "Error: File does exists."
+fi
+if [ "`cat "$path renamed"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path renamed"
+
+##########################################################
+echo "Moving to existing file"
+##########################################################
+echo $long > "$path 1" 
+echo $short > "$path 2" 
+if [ ! -f "$path 1" ] || [ ! -f "$path 2" ]
+then
+    echo "Error: File does not exists."
+fi
+mv "$path 1" "$path 2"
+if [ ! -f "$path 2" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ -f "$path 1" ]
+then
+    echo "Error: File does exists."
+fi
+if [ "`cat "$path 2"`" != "`echo $long`" ] 
+then
+	echo "Error: Files are not equal."
+	echo "is: ""`cat "$path 2"`"
+fi
+rm "$path 2"
+
+
+##########################################################
+echo "Copying empty file"
+##########################################################
+touch "$path"
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+cp "$path" "$path copy"
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ ! -f "$path copy" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path copy"`" != "" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path copy"
+rm "$path"
+
+##########################################################
+echo "Copying non empty file"
+##########################################################
+echo $long > "$path" 
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+cp "$path" "$path copy"
+if [ ! -f "$path" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ ! -f "$path copy" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path copy"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path"
+rm "$path copy"
+
+
+##########################################################
+echo "Copying to existing file"
+##########################################################
+echo $long > "$path 1" 
+echo $short > "$path 2" 
+if [ ! -f "$path 1" ] || [ ! -f "$path 2" ]
+then
+    echo "Error: File does not exists."
+fi
+cp "$path 1" "$path 2"
+if [ ! -f "$path 1" ] || [ ! -f "$path 2" ]
+then
+    echo "Error: File does not exists."
+fi
+if [ "`cat "$path 2"`" != "$(echo -en $long\\n)" ] 
+then
+	echo "Error: Files content changed."
+fi
+if [ "`cat "$path 2"`" != "`cat "$path 1"`" ] 
+then
+	echo "Error: Files are not equal."
+fi
+rm "$path 1"
+rm "$path 2"
+
+##########################################################
+echo "Listing directory?"
+##########################################################
+
+
