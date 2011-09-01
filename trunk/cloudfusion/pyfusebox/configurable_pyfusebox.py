00eddad0fe1f1946d85a010139b5ccf6acdc671a
nubi_mueller <nubi_mueller@da8baf20-f473-4a87-bbd7-1904b4eb812b>
2011-09-01 10:32:12 +0000

    Add logging
    git-svn-id: file:///home/josef/Work/repos/DBBM+Hiwis/nubi/_NUBICONV/foo@214 da8baf20-f473-4a87-bbd7-1904b4eb812b

--- trunk/cloudfusion/pyfusebox/configurable_pyfusebox.py
+++ trunk/cloudfusion/pyfusebox/configurable_pyfusebox.py
@@ -17,6 +17,8 @@ class ConfigurablePyFuseBox(PyFuseBox):
     def __init__(self, root, virtual_config_file='/.config/.###config###'):
         self.virtual_file = VirtualConfigFile(virtual_config_file)
         self.store_initialized = False
+        self.logger = logging.getLogger('pyfusebox')
+        self.logger.debug("initialized configurable pyfusebox")
         super( ConfigurablePyFuseBox, self ).__init__(root, None)
 
     def getattr_for_root(self):
@@ -28,7 +30,7 @@ class ConfigurablePyFuseBox(PyFuseBox):
         return st;
     
     def getattr(self, path, fh=None):
-        print "getattr "+path
+        self.logger.debug("getattr "+path+"")
         if path == "/": 
             return self.getattr_for_root();
         if path == self.virtual_file.get_path():
@@ -38,24 +40,29 @@ class ConfigurablePyFuseBox(PyFuseBox):
         raise FuseOSError(ENOENT)
     
     def truncate(self, path, length, fh=None):
+        self.logger.debug("truncate %s to %s" % (path, length))
         if path == self.virtual_file.get_path():
             self.virtual_file.truncate()
         if self.store_initialized:
             super( ConfigurablePyFuseBox, self ).truncate(path, length, fh)
     
     def rmdir(self, path):
+        self.logger.debug("rmdir %s" % (path))
         if self.store_initialized:
             super( ConfigurablePyFuseBox, self ).rmdir(path)
         
     def mkdir(self, path, mode):
+        self.logger.debug("mkdir %s with mode: %s" % (path, str(mode)))
         if self.store_initialized:
             super( ConfigurablePyFuseBox, self ).mkdir(path, mode)
 
     def statfs(self, path):#add size of vtf
+        self.logger.debug("statfs %s" % (path))
         if self.store_initialized:
             super( ConfigurablePyFuseBox, self ).statfs(path)
     
     def rename(self, old, new):
+        self.logger.debug("rename %s" % (old, new))
         """if new == self.virtual_file.get_path():
             buf = 
             written_bytes =  self.virtual_file.write(buf, offset)
@@ -66,6 +73,7 @@ class ConfigurablePyFuseBox(PyFuseBox):
             return super( ConfigurablePyFuseBox, self ).rename(old, new)
 
     def create(self, path, mode):
+        self.logger.debug("create %s with mode %s" % (path, str(mode)))
         if self.store_initialized:
             return super( ConfigurablePyFuseBox, self ).create(path, mode)
     
@@ -74,22 +82,28 @@ class ConfigurablePyFuseBox(PyFuseBox):
             super( ConfigurablePyFuseBox, self ).unlink(path)
 
     def read(self, path, size, offset, fh):
+        self.logger.debug("unlink %s" % path)
         if path == self.virtual_file.get_path():
             return self.virtual_file.read(size, offset)
         if self.store_initialized:
             return super( ConfigurablePyFuseBox, self ).read(path, size, offset, fh)
 
     def _initialize_store(self):
+        self.logger.debug("_initialize_store:")
         conf = self.virtual_file.get_store_config_data()
         service = conf['name']
+        self.logger.debug("got service name")
         cache_time = 0
         metadata_cache_time = 0
         if 'cache' in conf:
             cache_time = conf['cache'] 
         if 'metadata_cache' in conf:
             metadata_cache_time = conf['metadata_cache'] 
+        self.logger.debug("got cache parameter")
         auth = self.virtual_file.get_service_auth_data()
+        self.logger.debug("got auth data")
         store = self.__get_new_store(service, auth) #catch error?
+        self.logger.debug("initialized store")
         if cache_time > 0 and metadata_cache_time > 0:
             store = MetadataCachingStore( CachingStore( MetadataCachingStore( store, metadata_cache_time ), cache_time ), metadata_cache_time )
         elif cache_time > 0:
@@ -97,18 +111,25 @@ class ConfigurablePyFuseBox(PyFuseBox):
         elif metadata_cache_time > 0:
             store = MetadataCachingStore( store, metadata_cache_time )
         self.store = store
+        self.logger.debug("initialized service")
         self.store_initialized = True
         
     def __get_new_store(self, service, auth):
+        self.logger.debug("__get_new_store:")
         if service.lower() == "sugarsync":
             store = SugarsyncStore(auth)
         else: # default
+            self.logger.debug("get dropbox store")
             store = DropboxStore(auth)
+            self.logger.debug("got dropbox store")
         return store
     
     def write(self, path, buf, offset, fh):
+        self.logger.debug("write %s ... starting with %s at %s - fh: %s" % (path, buf[0:10], offset, fh))
         if path == self.virtual_file.get_path():
-            written_bytes =  self.virtual_file.write(buf, offset)
+            self.logger.debug("writing to virtual file "+path)
+            written_bytes = self.virtual_file.write(buf, offset)
+            self.logger.debug("written bytes:"+str(written_bytes))
             if written_bytes >0: # configuration changed
                 self._initialize_store()
             return written_bytes
@@ -117,12 +138,14 @@ class ConfigurablePyFuseBox(PyFuseBox):
         return 0
     
     def flush(self, path, fh):
+        self.logger.debug("flush %s - fh: %s" % (path, fh))
         if path == self.virtual_file.get_path():
             return 0
         if self.store_initialized:
             super( ConfigurablePyFuseBox, self ).flush(path, fh)
     
     def release(self, path, fh):
+        self.logger.debug("release %s - fh: %s" % (path, fh))
         if path == self.virtual_file.get_path():
             return 0
         if self.store_initialized:
