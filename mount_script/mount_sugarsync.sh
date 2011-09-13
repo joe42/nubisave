d78fd5ea915b8e6eeae58adfc6ef9acb5c98b53b
Josef Spillner <josef.spillner@tu-dresden.de>
2011-09-13 16:20:58 +0200

    - consolidate mount scripts

--- mount_script/mount_sugarsync.sh
+++ mount_script/mount_sugarsync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p ~/.cache/nubisave/storages/storage02/
-sudo umount ~/.cache/nubisave/storages/storage02/
+sudo umount ~/.cache/nubisave/storages/storage02/ 2>/dev/null
 python -m cloudfusion.main ~/.cache/nubisave/storages/storage02/ sugarsync Nubisave123 cache
