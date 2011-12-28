18863ab13abd3af4437ff6c554f6c02218c51301
Johannes Müller <dna42@gmx.de>
2011-12-28 17:09:19 +0100

    Delete obsolete mount scripts

--- mount_script/mount_dropbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.cache/nubisave/storages/storage01/
-sudo umount ~/.cache/nubisave/storages/storage01/ 2>/dev/null
-python -m cloudfusion.main ~/.cache/nubisave/storages/storage01/ dropbox nubisave123 cache
