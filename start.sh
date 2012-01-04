ec52ca6f249913903be713272189fd38fa382714
Johannes Müller <dna42@gmx.de>
2012-01-04 07:14:36 +0100

    Use symbolic link to nubisave's data directory in user's home directory to hide /config

--- start.sh
+++ start.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 echo "Start des Splitter Modules"
-mountpoint=$HOME/nubisavemount
+userdir=$HOME/nubisave
+mountpoint=$HOME/.nubisave/nubisavemount
 storages=$HOME/.nubisave/storages
 
-mkdir -p $mountpoint $storages
+mkdir -p "$mountpoint" "$storages"
 scriptpath=`readlink -f $0`
 scriptloc=`dirname $scriptpath`
 cd $scriptloc
 
 cd splitter
-./mount.sh $mountpoint $storages &
+./mount.sh "$mountpoint" "$storages" &
 cd ..
 
 if [ "$1" != "headless" ]
 then
     # FIXME: This should be solved by some event detection
     sleep 2;
-
+    if [ ! -h "$userdir" ]
+    then
+	ln -s "$mountpoint/data" "$userdir"
+    fi
     echo "Start von NubiSave"
 
     cd bin/
-    java -jar Nubisave.jar $mountpoint
+    java -jar Nubisave.jar "$mountpoint"
 fi
 
 
