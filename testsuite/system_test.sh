b52290d9b4a5b62b0af40fe010ed0c527c6a5e60
Johannes Müller <dna42@gmx.de>
2012-01-08 00:18:37 +0100

    Modify system tests

--- testsuite/system_test.sh
+++ testsuite/system_test.sh
@@ -16,23 +16,45 @@ NUBISAVE_DIR="$CURRENT_DIR/.."
 MOUNTPOINT=~/nubisavemount
 IOZONE_DIR="$2"
 STORAGES_DIR=~/.nubisave/storages
-echo logs are written to "$LOG_DIR"
+CONFIGURATION_DIR=~/.nubisave/db/splitter_configuration
 
 cd systemtest
+
+echo logs are written to "$LOG_DIR"
 mkdir -p "$LOG_DIR"
+
+echo write difference to "$LOG_DIR"/diff
 git diff > "$LOG_DIR"/diff
-"$NUBISAVE_DIR"/start.sh  &> "$LOG_DIR"/splitter_log &
+
+echo remove configuration directory $CONFIGURATION_DIR 
+rm -r $CONFIGURATION_DIR
+
+echo unmount Nubisave and clean storage directory "$STORAGES_DIR"
+fusermount -zu "$MOUNTPOINT"
+find "$STORAGES_DIR" -type d -exec fusermount -u {} -z \\;
+rm -r "$STORAGES_DIR"/*
+
+echo start Nubisave: "$NUBISAVE_DIR"/start.sh
+"$NUBISAVE_DIR"/start.sh  &> "$LOG_DIR"/splitter_log1 &
+
+echo compile and start GUI configuration
 javac -classpath lib/sikuli-script.jar src/GuiTest.java
 java -classpath "lib/sikuli-script.jar:src/" GuiTest
 cd ..
 
-#function tests:
-"$CURRENT_DIR"/fuse_tests.sh "$MOUNTPOINT"/data/ mytestfile | tee "$LOG_DIR"/fuse_tests_results
-#performance tests for memory and write/read:
+echo write testfile to "$MOUNTPOINT"/test
+echo test > "$MOUNTPOINT"/test
+
+
+
+echo file system tests: "$CURRENT_DIR"/fuse_tests.sh
+"$CURRENT_DIR"/fuse_tests.sh "$MOUNTPOINT"/ mytestfile | tee "$LOG_DIR"/fuse_tests_results
+
+echo performance tests for memory and write/read:
 rm -f /tmp/nubi_mem_trace 
 touch /tmp/nubi_mem_trace 
 ./mem.sh /tmp/nubi_mem_trace & #-q 64 -g 132000
-"$IOZONE_DIR"/iozone -acMe -y 4 -g 14000 -f "$MOUNTPOINT"/data/.iozone |tee "$LOG_DIR"/iozone_results
+"$IOZONE_DIR"/iozone -acMed -y 4 -g 14000 -f "$MOUNTPOINT"/data/.iozone |tee "$LOG_DIR"/iozone_results
 pkill mem.sh
 cat /tmp/nubi_mem_trace > "$LOG_DIR"/nubi_mem_trace
 cat /tmp/nubi_mem_trace | grep java > /tmp/nubi_mem_trace2
@@ -60,7 +82,28 @@ mv write "$LOG_DIR"
 mv write_telemetry "$LOG_DIR"
 mv read_telemetry "$LOG_DIR"
 
+echo unmount Nubisave and clean storagedirectory "$STORAGES_DIR"
+fusermount -zu "$MOUNTPOINT"
+find "$STORAGES_DIR" -type d -exec fusermount -u {} -z \\;
+rm -r "$STORAGES_DIR"/*
 
+echo restart Nubisave: "$NUBISAVE_DIR"/start.sh
+"$NUBISAVE_DIR"/start.sh  &> "$LOG_DIR"/splitter_log2 &
+
+echo mount the pre-configured components
+javac -classpath lib/sikuli-script.jar src/MountAll.java
+java -classpath "lib/sikuli-script.jar:src/" MountAll
+
+echo check if test file exists after restart
+if [ "`cat "$MOUNTPOINT"/test`" != "$(echo -en test\\n)" ] 
+then
+    echo "Error: Files content changed."
+fi
+
+echo remove test file
+rm "$MOUNTPOINT"/test
+
+echo unmount Nubisave and clean storagedirectory "$STORAGES_DIR"
 fusermount -zu "$MOUNTPOINT"
 find "$STORAGES_DIR" -type d -exec fusermount -u {} -z \\;
 rm -r "$STORAGES_DIR"/*
--- testsuite/systemtest/src/GuiTest.java
+++ testsuite/systemtest/src/GuiTest.java
@@ -5,24 +5,8 @@ import java.util.NoSuchElementException;
 
 import org.python.modules.math;
 import org.sikuli.script.*;
-public class GuiTest {
-	private static final String CUSTOM_SERVICE_DIALOG_OK_BTN = "img/custom_service_dialog_ok_btn.png";
-	private static final String CUSTOM_SERVICE_DIALOG_FILENAME_FIELD = "img/custom_service_dialog_filename_field.png";
-	private static final String OPTIONS_TAB = "img/Options_tab.png";
-	private static final String OPEN_MOUNT_DIRECTORY_BTN = "img/Open_mount_directory_btn.png";
-	private static final String DROPBOX_CONFIG_BTN = "img/Dropbox_User_Pass_btn.png";
-	private static final String SUGARSYNC_INI_PATH = "/home/joe/Sugarsync.ini";
-	private static final String DROPBOX_INI_PATH = "/home/joe/Dropbox.ini";
-	private static final String ENCFS_INI_PATH = "/home/joe/workspace/nubisave/splitter/mountscripts/encfs.ini";
-	private static final String DIRECTORY_INI_PATH1 = "/home/joe/directory1.ini";
-	private static final String DIRECTORY_INI_PATH2 = "/home/joe/directory2.ini";
-	private static final String DIRECTORY_INI_PATH3 = "/home/joe/directory3.ini";
-	private static final String CUSTOM_SERVICE_DIALOG = "img/custom_service_dialog.png";
-	private static final String CUSTOM_SERVICE_BTN = "img/custom.png";
-	private static final String MOUNTED_CHECKBOX = "img/mounted_checkbox.png";
-	private static final String MOUNTED_CHECKBOX_CHECKED = "img/mounted_checkbox_checked.png";
-	private static final String NUBISAVE_MENU_BAR = "img/nubisave_menu_bar.png";
-	private static final String MENU_BAR_CLOSE = "img/menu_bar_close.png";
+public class GuiTest implements Constants {
+
 	
 	Screen screen;
 	public GuiTest(){
@@ -41,32 +25,15 @@ public class GuiTest {
 			chooseCustomService(DIRECTORY_INI_PATH2);
 			chooseCustomService(DIRECTORY_INI_PATH3);
 			
-			//screen.find(DROPBOX_CONFIG_BTN);
-			//screen.getLastMatch().click(CONFIG_BTN);
-			//screen.wait(CONFIG_DIALOG,10);
-			//screen.type(null, "hello world\thohoho", 0);
+			new MountAll().run();
 			
-			screen.click(MOUNTED_CHECKBOX,0);
-			screen.wait(MOUNTED_CHECKBOX_CHECKED,10);
-			screen.click(MOUNTED_CHECKBOX,0);
-			Thread.sleep(2000);
-			screen.click(MOUNTED_CHECKBOX,0);
-			Thread.sleep(2000);
-			screen.click(OPTIONS_TAB,0);
-			Thread.sleep(1000);
-			screen.click(OPEN_MOUNT_DIRECTORY_BTN,0);
-			Thread.sleep(5000);
-			screen.find(NUBISAVE_MENU_BAR).click(MENU_BAR_CLOSE);
-			/**
-			Iterator<Match> matches = screen.findAll(MOUNTED_CHECKBOX_CHECKED);
-			int waitXTimes = 10;
-			while(getNrOfMatches(matches) != 2 && waitXTimes > 0){
-				matches = screen.findAll(MOUNTED_CHECKBOX_CHECKED);
-				waitXTimes--;
-			}
-			if(getNrOfMatches(matches) != 2){
-				throw new NoSuchElementException("At least one store is not mounted successfully."+getNrOfMatches(matches));
-			}*/
+			//screen.click(OPTIONS_TAB,0);
+			//Thread.sleep(1000);
+			//screen.click(OPEN_MOUNT_DIRECTORY_BTN,0);
+			//Thread.sleep(5000);
+			screen.rightClick(LOGO);
+			screen.click(MENU_EXIT);
+			
 			
 		} catch (FindFailed e) {
 			// TODO Auto-generated catch block
@@ -74,14 +41,7 @@ public class GuiTest {
 		}
 		
 	}
-	private int getNrOfMatches(Iterator<Match> matches){
-		int matchesCnt = 0;
-		while(matches.hasNext()){
-			matches.next();
-			matchesCnt++;
-		}
-		return matchesCnt;
-	}
+	
 	private void chooseCustomService(String serviceConfigurationFilePath) throws FindFailed {
 		/**Adds the custom service to the services table specified by the image located at the path service. */
 		screen.wait(CUSTOM_SERVICE_BTN,10);
