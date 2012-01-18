7a8bfd0f0d78aa425fff189728a2b3e32826477e
Johannes Müller <dna42@gmx.de>
2012-01-18 17:11:06 +0100

    Bugfix change potential data storage strategies every time the storage services change

--- splitter/src/com/github/joe42/splitter/storagestrategies/StorageStrategyFactory.java
+++ splitter/src/com/github/joe42/splitter/storagestrategies/StorageStrategyFactory.java
@@ -3,6 +3,8 @@ package com.github.joe42.splitter.storagestrategies;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import com.github.joe42.splitter.backend.BackendService;
 import com.github.joe42.splitter.backend.BackendServices;
@@ -16,7 +18,7 @@ public class StorageStrategyFactory {
 	private BackendServices services;
 	private boolean changeToCurrentStrategy = true;
 	private StorageStrategy previousStorageStrategy = null;
-	
+	private Set<String> dataDirPaths = new TreeSet<String>();
 	public StorageStrategyFactory(BackendServices services){
 		this.services = services;
 	}
@@ -30,8 +32,12 @@ public class StorageStrategyFactory {
 	public StorageStrategy createStrategy(String strategyName, int redundancy) {
 		StorageStrategy ret = null;
 		if(strategyName.equals("RoundRobin")){
-			if(roundRobin == null){
+			if(roundRobin == null || ! dataDirPaths.equals(new TreeSet<String>(services.getDataDirPaths()))){
+				dataDirPaths = new TreeSet<String>(services.getDataDirPaths());
 				roundRobin = new RoundRobinStorageStrategy(services.getDataDirPaths());
+			}
+			if( !new TreeSet<String>(roundRobin.getPotentialStorageDirectories()).equals(new TreeSet<String>(services.getDataDirPaths())) ) {
+				roundRobin.setPotentialStorageDirectories(services.getDataDirPaths());
 			}
 			roundRobin.setRedundancy(redundancy);
 			changeToCurrentStrategy = roundRobin.changeToCurrentStrategy(previousStorageStrategy);
