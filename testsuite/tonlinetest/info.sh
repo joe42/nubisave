a344184f85870bbac04f0323ceb1fe15a60e7525
Johannes Müller <dna42@gmx.de>
2012-01-30 14:19:50 +0100

    Add test scripts for tonline webdav service

---
+++ testsuite/tonlinetest/info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "tonline webdav at https://webdav.mediencenter.t-online.de/ date:" `date`
+echo "NubiSave revision: "
+git rev-parse HEAD
+echo "\n\n\n#############################	start ~/.davfs2/davfs2.conf	#############################\n"
+cat ~/.davfs2/davfs2.conf
+echo "\n#############################	end ~/.davfs2/davfs2.conf	#############################\n\n\n"
+echo "\n\n\n#############################	start ~/.davfs2/davfs2.conf	#############################\n"
+cat ~/.davfs2/davfs2.conf
+echo "\n#############################	end ~/.davfs2/davfs2.conf	#############################\n\n\n"
