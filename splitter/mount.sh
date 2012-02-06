6ed658d961b37abeddf562a24e173f399b70ca5b
Johannes Müller <dna42@gmx.de>
2012-02-06 15:54:35 +0100

    Set default logging level to INFO 	Modify splitter/mount.sh

--- splitter/mount.sh
+++ splitter/mount.sh
@@ -11,7 +11,7 @@ mkdir -p ~/.splitter/db/ #create log director
 LD_LIBRARY_PATH=../fusej/jni:$FUSE_HOME/lib $JDK_HOME/bin/java \
    -classpath ./build:./lib/*:../jigdfs/lib/*:../fusej/lib/* \
    -Dorg.apache.commons.logging.Log=fuse.logging.FuseLog \
-   -Dfuse.logging.level=DEBUG \
+   -Dfuse.logging.level=INFO \
    com.github.joe42.splitter.Main -f -s $1 $2
 
 find $2 -type d -exec fusermount -u {} -z \; 
