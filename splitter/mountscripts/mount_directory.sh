1795baac946b58ccac7ed1ca2d54f6883a2cb590
Johannes Müller <dna42@gmx.de>
2012-03-11 19:58:19 +0100

    Fix only create link to directory if it does not exist

--- splitter/mountscripts/mount_directory.sh
+++ splitter/mountscripts/mount_directory.sh
@@ -7,7 +7,7 @@ fi
 #make directory at mountpoint if it does not yet exist
 mkdir -p "$1"
 #link to directory
-if [ ! -h "$2" ]; then
+if [ ! -h "$1/data" ]; then
     ln -s "$2" "$1/data"
 fi
 #create a config file to let the splitter know everything went well
