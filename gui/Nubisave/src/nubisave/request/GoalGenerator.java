e0f280a8899202f2b5987a77b56ad2f478e4eef8
Josef Spillner <josef.spillner@tu-dresden.de>
2012-03-18 21:04:28 -0300

    - experimental adaptation of the goal generator to the new ontology

--- gui/Nubisave/src/nubisave/request/GoalGenerator.java
+++ gui/Nubisave/src/nubisave/request/GoalGenerator.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package nubisave.request;
 
 import java.util.Random;
@@ -25,110 +21,111 @@ public class GoalGenerator {
         String name = "Goal" + (new Random()).nextInt(100000000);
         wsmogoal.setName(name);
         wsmogoal.addNameSpace("own", "http://localhost:8080/Matchmaker/ontologies/goals/" + name + ".wsml#");
-        wsmogoal.addNameSpace("dc", "http://purl.org/dc/elements/1.1#");
         wsmogoal.addNameSpace("rank", "http://localhost:8080/Matchmaker/ontologies/Common/Ranking.wsml#");
-        wsmogoal.addNameSpace("cloudqos", "http://localhost:8080/Matchmaker/ontologies/CloudQoS.wsml#");
+        wsmogoal.addNameSpace("cloud", "http://localhost:8080/Matchmaker/ontologies/CloudStorage.wsml#");
         wsmogoal.addNameSpace("qosbase", "http://localhost:8080/Matchmaker/ontologies/QoSBase.wsml#");
+        wsmogoal.addNameSpace("rqos", "http://localhost:8080/Matchmaker/ontologies/RemoteQoSBase.wsml#");
+        wsmogoal.addNameSpace("biz", "http://localhost:8080/Matchmaker/ontologies/BusinessBase.wsml#");
         wsmogoal.addNameSpace("wsml", "http://www.wsmo.org/wsml/wsml-syntax#");
 
-        wsmogoal.addGoalOntologie("http://localhost:8080/Matchmaker/ontologies/CloudQoS.wsml#");
-       
-        wsmogoal.setCapability("postcondition definedBy ?serviceType memberOf cloudqos#CloudStorage");
-        
-        wsmogoal.addOntologie("http://localhost:8080/Matchmaker/ontologies/CloudQoS.wsml#");
+        wsmogoal.addGoalOntologie("http://localhost:8080/Matchmaker/ontologies/CloudStorage.wsml#");
+
+        wsmogoal.setCapability("postcondition definedBy ?serviceType memberOf cloud#CloudStorage");
+
+        wsmogoal.addOntologie("http://localhost:8080/Matchmaker/ontologies/CloudStorage.wsml#");
         wsmogoal.addOntologie("http://localhost:8080/Matchmaker/ontologies/Common/Ranking.wsml#");
-        
+
         String iname;
         iname = "instAvailability";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#Availability");
+        wsmogoal.getInstance(iname).addConcept("cloud#Availability");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getAvailabilityTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#Percentage");
-        
+
         iname = "instPricePerMonth";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#PricePerMonth");
+        wsmogoal.getInstance(iname).addConcept("cloud#PricePerMonth");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getPricePerMonthTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#Euro");
-        
+
         iname = "instNetworkBandwidth";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#NetworkBandwidth");
+        wsmogoal.getInstance(iname).addConcept("cloud#NetworkBandwidth");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getBandwidthTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#"+dialog.getBandwidthUnitCB().getSelectedItem());
-        
+
         iname = "instMaxDownTime";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#MaxDownTime");
+        wsmogoal.getInstance(iname).addConcept("cloud#MaxDownTime");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getMaxDownTimeTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#"+dialog.getMaxDownTimeUnitCB().getSelectedItem());
-        
+
         iname = "instPricePerData";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#PricePerData");
+        wsmogoal.getInstance(iname).addConcept("cloud#PricePerData");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getPricePerDataTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#Euro");
-        
+
         iname = "instResponseTime";
         wsmogoal.addInstance(iname);
-        wsmogoal.getInstance(iname).addConcept("cloudqos#ResponseTime");
+        wsmogoal.getInstance(iname).addConcept("cloud#ResponseTime");
         wsmogoal.getInstance(iname).addConcept("qosbase#GoalRequirement");
         wsmogoal.getInstance(iname).addParam("qosbase#value", dialog.getResponseTimeTextField().getText());
         wsmogoal.getInstance(iname).addParam("qosbase#unit", "qosbase#"+dialog.getResponseTimeUnitCB().getSelectedItem());
-        
+
         iname = "instResponseTimeConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#ResponseTime");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#ResponseTime");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getResponseTimeWeightCB().getSelectedIndex()+1+".0");
         wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getResponseTimeCheckBox().isSelected()?"1.0":"0.0");
-        
+
         iname = "instPricePerMonthConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#PricePerMonth");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#PricePerMonth");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getPricePerMonthWeightCB().getSelectedIndex()+1+".0");
         wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getPricePerMonthCheckBox().isSelected()?"1.0":"0.0");
-        
+
         iname = "instMaxDownTimeConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#MaxDownTime");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#MaxDownTime");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getMaxDownTimeWeightCB().getSelectedIndex()+1+".0");
-        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getPricePerMonthCheckBox().isSelected()?"1.0":"0.0");       
-                
+        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getPricePerMonthCheckBox().isSelected()?"1.0":"0.0");
+
         iname = "instAvailabilityConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#Availability");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#Availability");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getAvailabilityWeightCB().getSelectedIndex()+1+".0");
-        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getAvailabilityCheckBox().isSelected()?"1.0":"0.0");            
-         
+        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getAvailabilityCheckBox().isSelected()?"1.0":"0.0");
+
         iname = "instPricePerDataConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#PricePerData");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#PricePerData");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getPricePerDataWeightCB().getSelectedIndex()+1+".0");
-        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getPricePerDataCheckBox().isSelected()?"1.0":"0.0");  
-        
+        wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getPricePerDataCheckBox().isSelected()?"1.0":"0.0");
+
         iname = "instNetworkBandwidthConfiguration";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSConceptConfiguration");
         wsmogoal.getInstance(iname).addParam("rank#hasReputationScore", "1.0");
-        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloudqos#NetworkBandwidth");
+        wsmogoal.getInstance(iname).addParam("rank#hasQoSConceptIRI", "cloud#NetworkBandwidth");
         wsmogoal.getInstance(iname).addParam("rank#hasWeight", dialog.getBandwidthWeightCB().getSelectedIndex()+1+".0");
         wsmogoal.getInstance(iname).addParam("rank#hasMatchingThreshold", dialog.getBandwidthCheckBox().isSelected()?"1.0":"0.0");
-      
+
         iname = "ranking";
         wsmogoal.addInstance(iname);
         wsmogoal.getInstance(iname).addConcept("rank#QoSRankingScoreThreshold");
@@ -137,7 +134,7 @@ public class GoalGenerator {
         wsmogoal.getInstance(iname).addParam("rank#hasEqualPartialScore","1.0");
         wsmogoal.getInstance(iname).addParam("rank#hasLowerPartialScore", "0.0");
         wsmogoal.getInstance(iname).addParam("rank#hasHigherRankThreshold", "0.15");
-        
+
         return wsmogoal.generateGoal();
     }
 
