67e156c70a281979fbe37b15623ac6ee613a70d2
Johannes Müller <dna42@gmx.de>
2012-03-19 17:35:41 +0100

    Optimize splitter by tuning fuse options for big reads and writes

--- splitter/mount.sh
+++ splitter/mount.sh
@@ -12,7 +12,7 @@ LD_LIBRARY_PATH=../fusej/jni:$FUSE_HOME/lib $JDK_HOME/bin/java \
    -classpath ./build:./lib/*:../jigdfs/lib/*:../fusej/lib/* \
    -Dorg.apache.commons.logging.Log=fuse.logging.FuseLog \
    -Dfuse.logging.level=INFO \
-   com.github.joe42.splitter.Main -f -s $1 $2
+   com.github.joe42.splitter.Main -f -s -obig_writes,max_read=131072,max_write=131072 $1 $2
 
 find $2 -type d -exec fusermount -u {} -z \; 
 fusermount -u $1 -z;
