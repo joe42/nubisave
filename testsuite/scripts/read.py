62edc9285e111e3fb35b0b96206bf561f4d32043
Johannes Müller <dna42@gmx.de>
2013-08-29 22:52:04 +0200

    Bugfix scripts read.py, and write.py, open file in binary mode for compatibility

--- testsuite/scripts/read.py
+++ testsuite/scripts/read.py
@@ -4,7 +4,7 @@ import sys
 if len(sys.argv) != 4:
 	print "Read a number bytes at a position from a file."
 	print "Usage: %s filename position_in_bytes nr_of_bytes_to_read" % (sys.argv[0])
-with open(sys.argv[1],"r") as f:
+with open(sys.argv[1],"rb") as f:
     f.seek(int(sys.argv[2]))
     for i in range(1,int(sys.argv[3])):
         f.read(1),
