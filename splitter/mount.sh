5ddd947a7ede3fd1bb97af166d7ca729554ae158
Josef Spillner <josef.spillner@tu-dresden.de>
2013-09-08 14:16:00 +0200

    - unmounting is now mostly handled by the splitter itself; leaving these quiet invocations just for safety

--- splitter/mount.sh
+++ splitter/mount.sh
@@ -6,7 +6,7 @@
 # Don't assume build variables at runtime
 #. ../fusej/build.conf
 
-find $2 -mindepth 1 -type d -exec fusermount -u {} -z \;
+find $2 -mindepth 1 -type d -exec fusermount -u {} -z -q \;
 fusermount -u $1 -z -q
 
 if [ -d $JDK_HOME ] && [ -x $JDK_HOME/bin/java ]
@@ -22,5 +22,5 @@ LD_LIBRARY_PATH=../fusej/jni:$FUSE_HOME/lib $java \
    -Dfuse.logging.level=INFO \
    com.github.joe42.splitter.Main -f -s -obig_writes,max_read=131072,max_write=131072 $1 $2
 
-find $2 -mindepth 1 -type d -exec fusermount -u {} -z \;
-fusermount -u $1 -z
+find $2 -mindepth 1 -type d -exec fusermount -u {} -z -q \;
+fusermount -u $1 -z -q
