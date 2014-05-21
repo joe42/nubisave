29393aef37d36c7b0f58a22661cb1ecc4794fbaa
Travis <travis@travis-ci.org>
2014-05-21 12:21:46 +0200

    Modify class OptimalCauchyReedSolomonSplitter, to be a subclass of CauchyReedSolomonSplitter Bugfix method crsSplitFileOptimally of class OptimalCauchyReedSolomonSplitter

--- splitter/src/com/github/joe42/splitter/OptimalCauchyReedSolomonSplitter.java
+++ splitter/src/com/github/joe42/splitter/OptimalCauchyReedSolomonSplitter.java
@@ -1,9 +1,11 @@
 package com.github.joe42.splitter;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.*;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.bouncycastle.crypto.Digest;
@@ -29,7 +31,7 @@ import eu.vandertil.jerasure.jni.Jerasure;
 
 
 
-public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to CauchyReedSolomonSplitter and abstract interface
+public class OptimalCauchyReedSolomonSplitter extends CauchyReedSolomonSplitter { //Rename to CauchyReedSolomonSplitter and abstract interface
 	private static final int CAUCHY_WORD_LENGTH = 8; //assert( (fragmentsize % (packetsize * w)) == 0 ) ; assert( 2^w > 100 )
 	private static final Logger log = Logger.getLogger("CauchyReedSolomonSplitter");
 	private static final int BLOCKSIZE = 16; //jerasure_bitmatrix_encode - packetsize() % sizeof(long) != 0
@@ -47,6 +49,7 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 	
 	
 	public OptimalCauchyReedSolomonSplitter(BackendServices services){
+		super(services);
 		System.loadLibrary("Jerasure.jni");
 		this.services = services;
 		redundancy = 50;
@@ -59,58 +62,10 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 	}
 	
 	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#getRedundancy()
-	 */
-	@Override
-	public int getRedundancy() {
-		return redundancy;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#setRedundancy(int)
-	 */
-	@Override
-	public void setRedundancy(int redundancy) {
-		this.redundancy = redundancy;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#getStorageStrategyName()
-	 */
-	@Override
-	public String getStorageStrategyName() {
-		return storageStrategyName;
-	}
-	
-	public StorageStrategyFactory getStorageStrategyFactory(){
-		return storageStrategyFactory;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#setStorageStrategyName(java.lang.String)
-	 */
-	@Override
-	public void setStorageStrategyName(String storageStrategyName) {
-		if(storageStrategyName == null){
-			storageStrategyName = "";
-		}
-		this.storageStrategyName = storageStrategyName;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#getBackendServices()
-	 */
-	@Override
-	public BackendServices getBackendServices(){
-		return services;
-	}
-	
-	/* (non-Javadoc)
 	 * @see com.github.joe42.splitter.Splitter#splitFile(com.github.joe42.splitter.FileFragmentMetaDataStore, java.lang.String, java.nio.channels.FileChannel)
 	 */
 	@Override
 	public FileFragments splitFile(FileFragmentMetaDataStore fileFragmentMetaDataStore, String path, FileChannel temp) throws FuseException, IOException {
-		//In Subclass: if storageStrategyName.equals(StorageStrategyFactory.100_splits): do this else call superclass method
 		int nr_of_file_parts_successfully_stored = 0;
 		HashMap<String, byte[]> fileParts = new HashMap<String, byte[]>();
 		ArrayList<String> fragmentPaths = new ArrayList<String>();
@@ -142,34 +97,19 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 			nr_of_redundant_fragments = nr_of_file_fragments - nr_of_file_fragments_required;
 			nrOfRequiredSuccessfullyStoredFragments = fileFragmentMetaDataStore.getNrOfRequiredSuccessfullyStoredFragments(path);
 		}
-		System.out.println("required:"+nrOfRequiredSuccessfullyStoredFragments);
-		logStoreProperties(nr_of_file_fragments, nr_of_file_fragments_required, nr_of_redundant_fragments, nrOfRequiredSuccessfullyStoredFragments);
-		if(nr_of_file_fragments_required == 1){
-			fileParts = replicateFile( temp, fragmentPaths);
-		} else if(nr_of_file_fragments_required == nr_of_file_fragments){ //no redundancy but several stores
-			log.debug(" 1 . temp.size():"+ temp.size());
-			fileParts = simpleSplitFile( temp, fragmentPaths);
-		} else {
-			
-			crsSplitFile(temp, fileParts, fragmentPaths,
-					nr_of_file_fragments, nr_of_redundant_fragments);
+		if( ! (storageStrategy instanceof OptimalRedundancyStategy) || nr_of_file_fragments_required == 1 || nr_of_file_fragments_required == nr_of_file_fragments ) {
+			return super.splitFile(fileFragmentMetaDataStore, path, temp);
 		}
+		logStoreProperties(nr_of_file_fragments, nr_of_file_fragments_required, nr_of_redundant_fragments, nrOfRequiredSuccessfullyStoredFragments);
+		crsSplitFileOptimally(temp, fileParts, fragmentPaths, nr_of_file_fragments, nr_of_redundant_fragments);
 		log.debug(" temp.size():"+ temp.size());
 		MultipleFiles multipleFiles = concurrent_multi_file_handler.writeFilesAsByteArrays(fileParts);
 		nr_of_file_parts_successfully_stored = multipleFiles.getNrOfSuccessfullyTransferedFiles();
-
-		  log.debug("nr_of_file_parts_successfully_stored: "+nr_of_file_parts_successfully_stored+" - MAX_FILE_FRAGMENTS_NEEDED "+nr_of_file_fragments_required);
-		//TODO: replace with nrOfRequiredSuccessfullyStoredFragments
-		if (nr_of_file_parts_successfully_stored < nr_of_file_fragments_required && ! (storageStrategy instanceof OptimalRedundancyStategy)) {
-			throw new FuseException(
-					"IO error: Not enough file parts could be stored.")
-					.initErrno(FuseException.EIO);
-		} else {
-			return new FileFragments(fragmentPaths, nr_of_file_fragments_required, nrOfRequiredSuccessfullyStoredFragments, multipleFiles.getChecksums(fragmentPaths), temp.size());
-		}
+		log.debug("nr_of_file_parts_successfully_stored: "+nr_of_file_parts_successfully_stored+" - MAX_FILE_FRAGMENTS_NEEDED "+nr_of_file_fragments_required);
+	    return new FileFragments(fragmentPaths, nr_of_file_fragments_required, nrOfRequiredSuccessfullyStoredFragments, multipleFiles.getChecksums(fragmentPaths), temp.size());
 	}
 
-	private void crsSplitFile(FileChannel temp, HashMap<String, byte[]> fileParts, List<String> fragmentFileNames, 
+	private void crsSplitFileOptimally(FileChannel temp, HashMap<String, byte[]> fileParts, List<String> fragmentFileNames, 
 			int nr_of_file_fragments, int nr_of_redundant_fragments) throws FuseException {
 		int w = CAUCHY_WORD_LENGTH; 
 	   	int[] bitmatrix; //keep matrices local for multithreaded access
@@ -217,9 +157,14 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 			int elements;
 			ByteBuffer chunk;
 			Map<String,Integer> elementsForStorages = strategy.getFragmentNameToNrOfElementsMap();
+			for(Entry<String, Integer> e : elementsForStorages.entrySet()){
+				log.debug("intend to write "+e.getValue()+" elements to " + new File(e.getKey()).getParent());
+			}
 			int i=0;
 			for(String fragment_name: fragmentFileNames) {
-				elements = elementsForStorages.get(fragment_name);
+				String storageDir = new File(fragment_name).getParent().toString();
+				System.out.println("storage directory "+storageDir);
+				elements = elementsForStorages.get(storageDir);
 				chunk = ByteBuffer.wrap(new byte[elements*data_ptrs[0].length]);
 				for(int j=0; j<elements; j++){
 					if( i < data_ptrs.length){
@@ -238,31 +183,6 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 			throw new FuseException("IDA could not be initialized")
 					.initErrno(FuseException.EACCES);
 		}
-		
-	}
-
-	private HashMap<String, byte[]> simpleSplitFile(FileChannel temp, List<String> fragmentFileNames) throws IOException {
-			log.debug("Splitting with no redundancy");
-			byte[] arr;
-			int size=0, read_bytes;
-			HashMap<String, byte[]> fileParts = new HashMap<String, byte[]>();
-			int normal_array_size = ((int) temp.size()+1)/fragmentFileNames.size();
-			arr = new byte[normal_array_size];
-			for (String fragmentFileName: fragmentFileNames) {
-				if(size+normal_array_size>temp.size()){
-					arr = new byte[(int)temp.size()-size];
-				} else {
-					arr = new byte[normal_array_size];
-				}
-				read_bytes = temp.read(ByteBuffer.wrap(arr));
-				size += read_bytes;
-				/*if(read_bytes <1){
-					break;
-				}*/
-				log.debug("write: " + fragmentFileName);
-				fileParts.put(fragmentFileName, arr);
-			}
-			return fileParts;
 	}
 
 	private void logStoreProperties(int nrOfFileFragments,
@@ -274,18 +194,6 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 		log.debug("nrOfRedundantFragments:" + nrOfRedundantFragments);
 		log.debug("nrOfRequiredSuccessfullyStoredFragments:" + nrOfRequiredSuccessfullyStoredFragments);
 		
-	}
-
-	private HashMap<String, byte[]> replicateFile(FileChannel temp, List<String> fragmentFileNames) throws IOException {
-		byte[] arr;
-		HashMap<String, byte[]> fileParts = new HashMap<String, byte[]>();
-		arr = new byte[(int) temp.size()];
-		temp.read(ByteBuffer.wrap(arr));
-		for (String fragmentFileName: fragmentFileNames) {
-			log.debug("write: " + fragmentFileName);
-			fileParts.put(fragmentFileName, arr);
-		}
-		return fileParts;
 	}
 
 	/* (non-Javadoc)
@@ -301,31 +209,19 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 			fragmentPathsToChecksum = zip(fileFragmentMetaDataStore.getFragments(path), fileFragmentMetaDataStore.getFragmentsChecksums(path)); 
 			int nr_of_redundant_fragments = fragmentNames.size() - nr_of_file_fragments_required;
 			logStoreProperties(fileFragmentMetaDataStore.getNrOfFragments(path), nr_of_file_fragments_required, nr_of_redundant_fragments, fileFragmentMetaDataStore.getNrOfRequiredSuccessfullyStoredFragments(path));
-			log.debug("read: " + fragmentNames);
-			if(nr_of_file_fragments_required == 1){
-				ret = new RandomAccessTemporaryFileChannel();
-				MultipleFiles multipleFiles = serial_multi_file_handler
-						.getFilesAsByteArrays(fragmentPathsToChecksum, nr_of_file_fragments_required);
-				log.debug(fileFragmentMetaDataStore.getNrOfFragments(path)+" "+fileFragmentMetaDataStore.getNrOfRequiredFragments(path));
-				if(multipleFiles.getNrOfSuccessfullyTransferedFiles() < 1){
-					throw new IOException("File could not be retrieved successfully.");
-				}
-				ret.getChannel().write(ByteBuffer.wrap(multipleFiles.getSuccessfullyTransferedFiles().get(0)));
-				return ret;
-			} else if(nr_of_file_fragments_required == fragmentNames.size()){ //no redundancy but several stores
-				return simpleGlueFilesTogether(fileFragmentMetaDataStore.getFragments(path), fileFragmentMetaDataStore.getFragmentsChecksums(path));
-			}else {
-				
-				return crsGlueFilesTogether(nr_of_redundant_fragments,
-						nr_of_file_fragments_required, fragmentNames, fragmentPathsToChecksum, (int) fileFragmentMetaDataStore.getFragmentsSize(path));
+			if( ! (storageStrategy instanceof OptimalRedundancyStategy) || nr_of_file_fragments_required == 1 || nr_of_file_fragments_required == fragmentNames.size() ) {
+				return super.glueFilesTogether(fileFragmentMetaDataStore, path);
 			}
+			log.debug("read: " + fragmentNames);
+     		return crsGlueFilesTogetherOptimally(nr_of_redundant_fragments,
+						nr_of_file_fragments_required, fragmentNames, fragmentPathsToChecksum, (int) fileFragmentMetaDataStore.getFragmentsSize(path));
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new FuseException("IO error", e).initErrno(FuseException.EIO);
 		}
 	}
 
-	private RandomAccessTemporaryFileChannel crsGlueFilesTogether(int nr_of_redundant_fragments,
+	private RandomAccessTemporaryFileChannel crsGlueFilesTogetherOptimally(int nr_of_redundant_fragments,
 			int nrOfDataElements,
 			List<String> fragmentNames, Map<String, byte[]> fragmentPathsToChecksum, int filesize)
 			throws FuseException, IOException {
@@ -424,10 +320,8 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 
 		ret = new RandomAccessTemporaryFileChannel(); //put data elements together into a single file
 		FileChannel fchan = ret.getChannel();
-		int elements = 0;
 		for(byte[] data: data_ptrs){
 			fchan.write(ByteBuffer.wrap(data));
-			elements++;
 		}
 		fchan.truncate(filesize);
 		return ret;
@@ -471,53 +365,4 @@ public class OptimalCauchyReedSolomonSplitter implements Splitter { //Rename to 
 		return fileNamesToChecksum;
 	}
 
-	/**
-	 * Glues files together in the order specified by their paths in <code>fragmentsPaths</code>
-	 * @param fragmentsPaths
-	 * @param fragmentsChecksums the checksum of each file fragment at the same index position as its path in fragmentsPaths
-	 * @return the complete file
-	 * @throws IOException
-	 */
-	private RandomAccessTemporaryFileChannel simpleGlueFilesTogether(
-			ArrayList<String> fragmentsPaths, List<byte[]> fragmentsChecksums) throws IOException {
-		RandomAccessTemporaryFileChannel ret;	
-		ret = new RandomAccessTemporaryFileChannel();
-		MultipleFiles multipleFiles = ((ConcurrentMultipleFileHandler) concurrent_multi_file_handler).
-			getFilesAsByteArrays(zip(fragmentsPaths, fragmentsChecksums));
-		if(fragmentsPaths.size() > multipleFiles.getNrOfSuccessfullyTransferedFiles()){
-			throw new IOException("Could not retrieve all fragments of the file.");
-		}
-		ByteBuffer bb;
-		for (String filePath: fragmentsPaths) {
-			bb = ByteBuffer.wrap(multipleFiles.getSuccessfullyTransferedFile(filePath));
-			ret.getChannel().write(bb);
-		}
-		return ret;
-	}
-
-	/* (non-Javadoc)
-	 * @see com.github.joe42.splitter.Splitter#getStorageAvailability()
-	 */
-	@Override
-	public double getStorageAvailability(){
-		storageStrategy = storageStrategyFactory.createStrategy(storageStrategyName, redundancy);
-		return storageStrategy.getStorageAvailability(); //forward call to the storage strategy factory
-	}
-
-	@Override
-	public void remove(String path) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void rename(String from, String to) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double getStorageRedundancy() {
-		return storageStrategy.getStorageRedundancy();
-	}
 }
