a2d7b3fd45b1ec31468bb917011bce154a01343d
Travis <travis@travis-ci.org>
2014-05-21 12:01:39 +0200

    Add feature Considering Storage Target Dependencies to script availability.py Bugfix script availability.py, for special case, when k is 1

--- splitter/availability.py
+++ splitter/availability.py
@@ -55,31 +55,33 @@ def powerset(some_list, k, m):
     But if m>k, then the subset contains the other elements in the powerset.
     some_list is a multiset'''
     n = k+m
-    if n == len(some_list): #if each store has one element
-        if m>k: # k is small, so there are many combinations with at least k elements - instead consider combinations with less than k elements
-            rng = range(0,k) # 0,1,2...k-1 stores (with less than k elements)
-        else: 
-            rng = range(k,len(some_list)+1) # k,k+1,k+2,...n stores (with more than k elements)
-        for r in rng:
-            for subset in combinations(some_list, r):
-                yield map(lambda x: x[0], subset)
-        return
-    if m>k: # k is small, so there are many combinations with at leas k elements - instead consider combinations with less than k elements
-        rng = range(0,k) # 0,1,2...k-1 stores (with less than k elements)
+    if m>k: # k is small, so there are many combinations with at least k elements - instead consider combinations with less than k elements
+        rng = range(0,k+1) # 0,1,2...k-1 stores (with less than k elements)
     else: 
         rng = range(1,len(some_list)+1) # 1,2...n stores (with more than k elements)
     for r in rng:
         for subset in combinations(some_list, r):
-            if m>k and not has_k_elements(k, subset):
+            if m>k and not has_k_elements(k, subset): 
                 yield map(lambda x: x[0], subset)
             elif m<=k and has_k_elements(k, subset):
                 yield map(lambda x: x[0], subset)
 
 def has_k_elements(k, store_list):
-    '''return: True iff at leas #k elements are in store_list'''
+    '''return: True iff at least #k elements are in store_list'''
     nr_of_elements = 0
-    for a, e in store_list:
-        nr_of_elements += e
+    ids = []
+    dependent_stores = [] 
+    for a, e, id, dependency in store_list:
+        ids.append(id)
+        if dependency:
+            dependent_stores.append((dependency,e))
+        else:
+            nr_of_elements += e
+        if nr_of_elements >= k:
+            return True
+    for dependency, e in dependent_stores:
+        if dependency in ids:
+            nr_of_elements += e
         if nr_of_elements >= k:
             return True
     return False
@@ -101,7 +103,13 @@ def sequence_generator(start_from_list=None):
         
 def get_availability(k, availabilities_list):
     result = 0 # availability
-    m = sum(e for a, e in availabilities_list) - k #number of coding elements
+    for i in range(len(availabilities_list)):
+        value = availabilities_list[i]
+        if len(value) == 2:
+            availabilities_list[i] = (value[0],value[1],i+1,0)
+        if len(value) == 3:
+            availabilities_list[i] = (value[0],value[1],i+1,value[2])
+    m = sum(e for a, e, id, dependency in availabilities_list) - k #number of coding elements
     n = k + m
     store_list = map(lambda x: x[0], availabilities_list)
     if len(availabilities_list) == 0:
@@ -131,7 +139,7 @@ def get_availability(k, availabilities_list):
 def main():        
     signal.signal(signal.SIGINT, signal_handler)
     k = int(sys.argv[1]) #number of elements sufficient for object availability
-    availabilities_list = eval(sys.argv[2]) #[(0.5,3), (0.5,1)]  
+    availabilities_list = eval(sys.argv[2]) #[(0.5,3), (0.5,1,0), (0.6,1,1)] # third store depends on first store; 0 as a third element of the tuple or only two elements in a tuple mean that they do not depend on another store
     
     print get_availability(k, availabilities_list)
     
