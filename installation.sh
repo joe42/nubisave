4e9f55df07ab3c0ef6f1b20de5c4d1c212381def
Travis <travis@travis-ci.org>
2014-05-29 01:08:03 +0200

    Modify installation.sh, require sudo as prerequisite Modify installation.sh, create /dev/fuse if required, and activate kernel module Modify installation.sh, make compatible with Travis.ci

--- installation.sh
+++ installation.sh
@@ -13,15 +13,17 @@ echo
 echo
 echo "Installiere Entwicklerpakete (openJDK, FUSE, ...)"
 
+set -x
+
+sudo apt-get update
 sudo apt-get install openjdk-7-jdk
-sudo apt-get install python-setuptools gcc libssl-dev python-dev
+sudo apt-get install python-setuptools gcc libssl-dev python-dev libfuse-dev
 sudo apt-get install fuse-utils || sudo apt-get install fuse #fuse-utils was renamed to fuse
 
-if [ ! -x /usr/bin/sudo ]; then
-    sudo usermod -a -G fuse "$USER"
-    su -c "./installation.sh" - "$USER" 
-fi
 
+sudo mknod /dev/fuse c 10 229
+sudo modprobe fuse
+ls -l /dev/fuse
 sudo chgrp fuse /dev/fuse   #On Debian: change permissions for fuse interface
 sudo chmod g+wr /dev/fuse   
 
@@ -45,4 +47,18 @@ sudo python setup.py install
 cd ..
 
 echo "Installation abgeschlossen"
+
+if groups "$USER" | grep -q -E ' fuse(\s|$)'; then
+    echo user is already in group fuse
+    exit
+fi
+if groups "$USER" | grep -q -E ' travis(\s|$)'; then
+    echo this is the travis testing environment. the correct user group will be effective when calling the test script
+    sudo usermod -a -G fuse "$USER"
+    exit
+else 
+    sudo usermod -a -G fuse "$USER"
+    sudo su - "$USER" 
+fi
+set +x
 
