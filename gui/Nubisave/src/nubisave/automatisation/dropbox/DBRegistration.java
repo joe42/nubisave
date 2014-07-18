a02a2a866e3db162444defdee33c5526cea19918
joe <dersinndeslebenz@gmx.net>
2014-07-18 16:08:50 +0200

    Modify class DBRegistration, add selection of (free) account type to complete the registration process

--- gui/Nubisave/src/nubisave/automatisation/dropbox/DBRegistration.java
+++ gui/Nubisave/src/nubisave/automatisation/dropbox/DBRegistration.java
@@ -10,6 +10,7 @@ import org.sikuli.script.*;
 public class DBRegistration {
 	protected final String dbRegistrationBtn = "/home/joe/projects/eclipse_ws/nubisave_clone/nubisave/gui/Nubisave/src/images/dropbox_img/Registration_Btn.png";
 	protected final String firstNameInput = "/home/joe/projects/eclipse_ws/nubisave_clone/nubisave/gui/Nubisave/src/images/dropbox_img/First_Name_Input.png";
+	protected final String freeAccountBtn = "/home/joe/projects/eclipse_ws/nubisave_clone/nubisave/gui/Nubisave/src/images/dropbox_img/Free_Accoun_Btn.png";
 	
 	Screen screen;
 	public DBRegistration(){
@@ -36,16 +37,23 @@ public class DBRegistration {
 			//Thread.sleep(5000);
 			//screen.rightClick(LOGO);
 			//screen.click(MENU_EXIT);
-			screen.paste("Johannes");
+			screen.paste("Raumschiff");
 			screen.type(Key.TAB);
-			screen.paste("Müller");
+			Thread.sleep(1000);
+			screen.paste("Galactica");
 			screen.type(Key.TAB);
-			screen.paste("email");
+			Thread.sleep(1000);
+			screen.paste("quirksquarks@web.de");
 			screen.type(Key.TAB);
+			Thread.sleep(1000);
 			screen.paste("123456");
 			screen.type(Key.TAB);
+			Thread.sleep(1000);
 			screen.type(Key.SPACE);
 			screen.click(dbRegistrationBtn,0);
+			screen.wait(freeAccountBtn);
+			Thread.sleep(3000);
+			screen.click(freeAccountBtn);
 		
 		} catch (FindFailed e) { 
 			// TODO Auto-generated catch block
