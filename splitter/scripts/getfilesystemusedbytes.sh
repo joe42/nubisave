8f7ce1789fdfb164cd95b238c9f12e991806bbad
Johannes Müller <quirksquarks@web.de>
2014-07-29 12:18:05 +0200

    Bugfix script getfilesystemusedbytes.sh, to get the used bytes of the file system at the path passed as the first argument

--- splitter/scripts/getfilesystemusedbytes.sh
+++ splitter/scripts/getfilesystemusedbytes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Output the file system's used bytes. The file system is given in the form of a path as the first parameter.
-echo `df . --block-size 1|tail -n1|awk '{print $3}'`
+echo `df $1 --block-size 1|tail -n1|awk '{print $3}'`
