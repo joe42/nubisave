59e14e7aeaad104fadff9fac3f19c92fee1dd6f8
Johannes Müller <quirksquarks@web.de>
2014-07-29 12:28:17 +0200

    Modify script create_streaming_files.sh, to add an optional file size unit

--- testsuite/scripts/create_streaming_files.sh
+++ testsuite/scripts/create_streaming_files.sh
@@ -7,7 +7,7 @@ if [ $# -lt 1 ]; then
    echo Creates n files {$size}MB_#NR for all sizes in test_file_sizes with the size suggested by the name. The content is random. 
    echo The number of files for each size is specified by the parameter number_of_files.
    echo The files are stored to the specified directory. All files in directory are deleted, when starting the script.
-   echo Usage: `basename $0` directory  test_file_sizes number_of_files
+   echo Usage: `basename $0` directory  test_file_sizes number_of_files [size_unit]
    echo Example: 
    echo Generate 10 1MB files, 10 10 MB files, 10 100MB files, 2 1000MB files and one 2000MB file.
    echo `basename $0` generated_files \"1 10 100 1000 2000\" \"10 10 10 2 1\" MB
@@ -18,6 +18,9 @@ fi
 FOLDER=$1
 FILE_SIZES=(`echo "$2"`)
 FILE_QUANTITY=(`echo "$3"`)
+if [ $# -lt 4 ]; then # if no unit is specified fall back to MB
+    UNIT='MB'
+fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p "$1"
 rm -f $FOLDER/*
@@ -26,6 +29,6 @@ for i in ${!FILE_SIZES[*]}  # iterate over all indexes in array
 do
 	for nr in `seq 1 ${FILE_QUANTITY[$i]}`  # from 1 to file quantity
 	do
-    	dd if=/dev/urandom of=$FOLDER/${FILE_SIZES[$i]}MB_$nr bs=1MB count=${FILE_SIZES[$i]}
+    	dd if=/dev/urandom of=$FOLDER/${FILE_SIZES[$i]}${UNIT}_$nr bs=1${UNIT} count=${FILE_SIZES[$i]}
 	done
 done
